24690bd3e343e1fe71bd7fc8ec2da520d133b5d6
Reshmi <rjawaharlal@dxc.com>
2020-07-28 17:52:17 +0530

    Web Api Demo

---
+++ WebApiDemo/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApiDemo.Models;
+using System.Web.Http;
+
+namespace WebApiDemo.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IEnumerable<Product> GetItems()
+        {
+            IList<Product> products = new List<Product>
+            {
+                new Product
+                {
+                    productName = "Biscuits",
+                    manufacturingYear = 2018,
+                    brandName="ParleG"
+                },
+                new Product
+                {
+                    productName = "Cars",
+                    manufacturingYear = 2018,
+                    brandName="BMW"
+                },
+                new Product
+                {
+                    productName = "Cars",
+                    manufacturingYear = 2018,
+                    brandName="Mercedese"
+                },
+                new Product
+                {
+                    productName = "Brush",
+                    manufacturingYear = 2017,
+                    brandName="Colgate"
+                }
+
+            };
+
+            return products;
+        }
+    }
+}
